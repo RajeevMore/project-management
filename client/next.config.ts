ed0c1b8915ac5138030510fadab3ad379ed5e883
Rajeev More <100182072+RajeevMore@users.noreply.github.com>
2025-03-15 11:56:26 +0530

    Update next.config.ts basepath

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: '/project',  // Ensure Next.js knows the correct subdirectory
+  trailingSlash: true,
 };
 
 export default nextConfig;
